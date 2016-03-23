a8d5df5057b7fb3fb3259c096486b94a54001155
mMjMm <gaaaxi@gmail.com>
2016-03-23 05:23:11 +0100

    smallest/largest with sort function

--- algorithm/array_get_smallest_largest.js
+++ algorithm/array_get_smallest_largest.js
@@ -2,6 +2,7 @@
 var array = [89, 19, 29, 39, 09, 2, 33, 56, 77, 23];
 var largest = 0;
 var smallest = array[0];
+
 document.writeln("");
 
 for (var i = 0; i <= largest; i++) {
@@ -13,7 +14,6 @@ for (var i = 0; i <= largest; i++) {
 
 }
 //finding smallest number in array
-
 for (var i = 1; i < array.length; i++) {
   if (array[i] < smallest) {
     smallest = array[i];
@@ -21,3 +21,20 @@ for (var i = 1; i < array.length; i++) {
 }
 document.writeln("Smallest number out of an array with an iteration:");
 document.writeln(smallest);
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+document.writeln("Largest number out of an array with sorting descending:");
+array.sort(function(a, b) {
+  return b - a // Sort the numbers in the array in descending order b-a
+});
+
+document.writeln(array[0]);
+document.writeln("Smallest number out of an array with sorting ascending:");
+
+array.sort(function(a, b) {
+  return a - b // Sort the numbers in the array in ascending order a-b
+});
+
+document.writeln(array[0]);
